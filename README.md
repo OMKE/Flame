99e28270e68db647b4bb4bab616046036ccefe87
Omar Iriskic <noisewavehd@gmail.com>
2019-04-16 03:12:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flame
+Flame is a MVC Micro-framework written in PHP.
+<p align=center>todo: write documentation</p>
---
+++ public/index.php
@@ -0,0 +1,9 @@
+
+<?php 
+/*
+** require_once bootstrap.php **
+desc:
+    We are including bootstrap.php to require all dependencies from app folder
+*/
+
+require_once '../app/bootstrap.php' ?>
